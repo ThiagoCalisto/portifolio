0336cf0927101186b5c1c70901228fd01ea2ed2b
ThiagoCalisto <warlocalgames@gmail.com>
2024-11-06 00:12:14 -0500

    adiciona bio

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { Container, Box, Heading, Image, Button } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, Button, Link } from '@chakra-ui/react'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import NextLink from 'next/link'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -39,13 +40,56 @@ const Page = () => {
           profissional. Estou ansioso para continuar contribuindo com minha experiência e habilidades,
           enquanto a empresa me fornece os recursos tecnológicos e humanos necessários para alcançar
           meu potencial máximo.</Paragraph>
-         <Box align="center" my={4}>
-         <NextLink legacyBehavior href="/works">
-         <Button rightIcon={<ChevronRightIcon />} colorScheme="teal" >
-         Meu Portifolio
-         </Button>
-         </NextLink>
-         </Box> 
+        <Box align="center" my={4}>
+          <NextLink legacyBehavior href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal" >
+              Meu Portifolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2000</BioYear>
+          Nasci em Brasilia, Brasil.
+        </BioSection>
+        <BioSection>
+          <BioYear>2018</BioYear>
+          Trabalhei como estagiario no Banco do Brasil
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Trabalhei como estagiario e fui logo efetivado para Desenvolvedor
+          Junior Back-end na CentralIT
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Finalizei o curso de Sistema de Informacao no IESB
+        </BioSection>
+        <BioSection>
+          <BioYear>2022 ate o presente</BioYear>
+          Trabalho como Desenvolvedor Fullstack na empresa TOQ solucoes
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          Eu ♡
+        </Heading>
+
+        <Paragraph>
+          <Link href="https://open.spotify.com/user/artefs?si=1b9103af5b534b84">
+            Musicas
+          </Link>
+          , Games, Animes, Desenhos,
+          <Link href="https://open.spotify.com/user/artefs?si=1b9103af5b534b84">
+            Fotografia
+          </Link>
+          , Tecnologia.
+        </Paragraph>
       </Section>
     </Container>
   )
---
+++ pages/works.js
@@ -0,0 +1,16 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
+
+import Section from "../components/section"
+
+const Works = () => {
+  return (
+    <Container>
+    <Heading as="h3" fontSize={20} mb={4}>
+    Trabalhos
+    </Heading>
+    </Container>
+    
+  )
+}
+
+export default Works
